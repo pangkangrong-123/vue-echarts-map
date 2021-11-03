9ebfdb9252d7e1beb3926381dd9504eb82dd7c23
pangkangrong <973534209@qq.com>
2021-11-03 14:36:26 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-11-03 11:10:34
+ * @LastEditTime: 2021-11-03 14:36:00
+ * @LastEditors: your name
+ * @Description: In User Settingsname
+ * @FilePath: \vue-echarts-map\src\App.vue
+-->
 <template>
   <div id="app">
     <router-view/>
@@ -6,6 +14,7 @@
 
 <script>
 export default {
+  name:"",
   name: "app",
 }
 </script>
